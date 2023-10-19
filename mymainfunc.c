01ae72e50d1113d6fd126f3278f114fa12f7eccc
cedrickkouek <cedrickkouek@gmail.com>
2023-10-19 19:02:46 +0100

    alright

--- mymainfunc.c
+++ mymainfunc.c
@@ -51,18 +51,37 @@ void parse_line(char *line, char **args)
  */
 void execute_command(char **args)
 {
-	pid_t pid = fork();
+	pid_t pid;
+	char *command_path;
+	char *path = _getenv("PATH");
 
+	checkEnv(args[0], path);
+
+	if (isEqual(args[0], "exit") == 0)
+	{
+		exit(0);
+	}
+
+	command_path = find_command(args[0]);
+	if (command_path == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		return;
+	}
+
+	pid = fork();
 	if (pid < 0)
 	{
 		perror("Fork Failed");
+		free(command_path);
 		exit(1);
 	}
 	else if (pid == 0)
 	{
-		if (execve(args[0], args, environ) < 0)
+		if (execve(command_path, args, environ) < 0)
 		{
 			perror(args[0]);
+			free(command_path);
 			exit(1);
 		}
 	}
@@ -70,6 +89,7 @@ void execute_command(char **args)
 	{
 		wait(NULL);
 	}
+	free(command_path);
 }
 
 /**
