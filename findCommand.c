63cfd2a8a47a80d6e8e8bdea6921807c20699ba4
cedrickkouek <cedrickkouek@gmail.com>
2023-10-19 20:39:04 +0100

    yes

--- findCommand.c
+++ findCommand.c
@@ -7,44 +7,42 @@
  */
 char *find_command(char *command)
 {
-	char *PATH;
-	char *p;
-	char *filepath;
-	struct stat st;
+    char *PATH;
+    char *p;
+    char *filepath;
+    struct stat st;
 
-	if (strchr(command, '/') != NULL)
-	{
-		struct stat st;
+    if (strchr(command, '/') != NULL)
+    {
+        if (stat(command, &st) == 0 && S_ISREG(st.st_mode)
+            && (st.st_mode & S_IXUSR))
+        {
+            char *filepath = malloc(strlen(command) + 1);
 
-		if (stat(command, &st) == 0 && S_ISREG(st.st_mode)
-				&& (st.st_mode & S_IXUSR))
-		{
-			char *filepath = malloc(strlen(command) + 1);
+            strcpy(filepath, command);
+            return (filepath);
+        }
 
-			strcpy(filepath, command);
-			return (filepath);
-		}
+        return (NULL);
+    }
 
-		return (NULL);
-	}
+    PATH = _getenv("PATH");
+    p = strtok(PATH, ":");
+    filepath = malloc(MAX_PATH_LENGTH + 1);
 
-	PATH = _getenv("PATH");
-	p = strtok(PATH, ":");
-	filepath = malloc(MAX_PATH_LENGTH + 1);
+    while (p != NULL)
+    {
+        snprintf(filepath, MAX_PATH_LENGTH + 1, "%s/%s", p, command);
+        if (stat(filepath, &st) == 0 && S_ISREG(st.st_mode)
+            && (st.st_mode & S_IXUSR))
+        {
+            return (filepath);
+        }
 
-	while (p != NULL)
-	{
-		snprintf(filepath, MAX_PATH_LENGTH + 1, "%s/%s", p, command);
-		if (stat(filepath, &st) == 0 && S_ISREG(st.st_mode)
-				&& (st.st_mode & S_IXUSR))
-		{
-			return (filepath);
-		}
+        p = strtok(NULL, ":");
+    }
 
-		p = strtok(NULL, ":");
-		filepath[MAX_PATH_LENGTH] = '\0';
-	}
-
-	free(filepath);
-	return (NULL);
+    free(filepath);
+    return (NULL);
 }
+
