e07e450c1347b598209b9e0c1cf1329607013f03
cedrickkouek <cedrickkouek@gmail.com>
2023-10-20 19:20:56 +0100

    VGood

--- mymainfunc.c
+++ mymainfunc.c
@@ -31,96 +31,84 @@ void checkEnv(char *str, char *path)
 
 void parse_line(char *line, char **args)
 {
-	char *token = strtok(line, " \n");
-	int i = 0;
+    char *token = strtok(line, " \n");
+    int i = 0;
 
-	while (token != NULL)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " \n");
-	}
+    while (token != NULL)
+    {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " \n");
+    }
 
-	args[i] = NULL;
+    args[i] = NULL;
 }
 
-/**
- *execute_command - executes a command
- *@args: arg1
- *Return: nothing
- */
 void execute_command(char **args)
 {
-	pid_t pid;
-	char *command_path;
-	char *path = _getenv("PATH");
+    pid_t pid;
+    char *command_path;
 
-	checkEnv(args[0], path);
+    if (strcmp(args[0], "exit") == 0)
+    {
+        exit(0);
+    }
 
-	if (isEqual(args[0], "exit") == 0)
-	{
-		exit(0);
-	}
+    command_path = find_command(args[0]);
+    if (command_path == NULL)
+    {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        return;
+    }
 
-	command_path = find_command(args[0]);
-	if (command_path == NULL)
-	{
-		fprintf(stderr, "%s: command not found\n", args[0]);
-		return;
-	}
-
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("Fork Failed");
-		free(command_path);
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		if (execve(command_path, args, environ) < 0)
-		{
-			perror(args[0]);
-			free(command_path);
-			exit(1);
-		}
-	}
-	else
-	{
-		wait(NULL);
-	}
-	free(command_path);
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("Fork Failed");
+        free(command_path);
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        if (execve(command_path, args, NULL) < 0)
+        {
+            perror(args[0]);
+            free(command_path);
+            exit(1);
+        }
+    }
+    else
+    {
+        wait(NULL);
+    }
+    free(command_path);
 }
 
-/**
- *main -  Is a super simple shell program
- *Return: An integer 0
- */
 int main(void)
 {
-	char *args[MAX_LINE / 2 + 1];
-	char line[MAX_LINE];
+    char *args[MAX_LINE / 2 + 1];
+    char line[MAX_LINE];
 
-	int interactive = isatty(STDIN_FILENO);
+    int interactive = isatty(STDIN_FILENO);
 
-	do {
-		if (interactive)
-		{
-			printf("$ ");
-			fflush(stdout);
-		}
+    do {
+        if (interactive)
+        {
+            printf("$ ");
+            fflush(stdout);
+        }
 
-		if (fgets(line, MAX_LINE, stdin) == NULL)
-		{
-			break;
-		}
+        if (fgets(line, MAX_LINE, stdin) == NULL)
+        {
+            break;
+        }
 
-		parse_line(line, args);
-		if (args[0] != NULL)
-		{
-			execute_command(args);
-		}
-	} while (interactive);
+        parse_line(line, args);
+        if (args[0] != NULL)
+        {
+            execute_command(args);
+        }
+    } while (interactive);
 
-	return (0);
+    return 0;
 }
