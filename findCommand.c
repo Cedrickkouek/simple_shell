bafc5f4bcce6fcd610928f005acc643bf96a2b15
Cedrickkouek <66407596+Cedrickkouek@users.noreply.github.com>
2023-10-20 19:36:14 +0200

    Add files via upload

--- findCommand.c
+++ findCommand.c
@@ -6,8 +6,14 @@
  */
 char *find_command(char *command)
 {
+	char *PATH, *p, *filepath, *path_copy;
 	struct stat st;
 
+	if (command == NULL)
+	{
+		fprintf(stderr, "Command not found\n");
+		return (NULL);
+	}
 	if (strchr(command, '/') != NULL)
 	{
 		if (stat(command, &st) == 0 && S_ISREG(st.st_mode) &&
@@ -18,10 +24,26 @@ char *find_command(char *command)
 			strcpy(filepath, command);
 			return (filepath);
 		}
-
 		return (NULL);
 	}
+	PATH = _getenv("PATH");
+	path_copy = _strdup(PATH);
 
+	p = strtok(path_copy, ":");
+	filepath = malloc(MAX_PATH_LENGTH + 1);
+	while (p != NULL)
+	{
+		snprintf(filepath, MAX_PATH_LENGTH + 1, "%s/%s", p, command);
+		if (stat(filepath, &st) == 0 && S_ISREG(st.st_mode) &&
+			(st.st_mode & S_IXUSR))
+		{
+			return (filepath);
+		}
+		p = strtok(NULL, ":");
+		filepath[MAX_PATH_LENGTH] = '\0';
+	}
+
+	free(path_copy);
+	free(filepath);
 	return (NULL);
 }
-
